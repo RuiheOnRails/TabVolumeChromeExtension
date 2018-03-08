cadd1b515fe4654af5af0f9061e9af84a674d450
Ruihe (Jerry) Li <ruiheli@uw.edu>
2018-03-08 12:31:40 -0800

    formatting

--- main.js
+++ main.js
@@ -1,32 +1,30 @@
 console.log("js loaded")
 
 
-let tabArray = chrome.tabs.query({},(tabs) => {
+let tabArray = chrome.tabs.query({}, (tabs) => {
   console.log(tabs);
   let container = document.getElementById("mainContainer")
   tabs.forEach((tab) => {
     if (tab.audible === true) {
       let title = tab.title
-
       let img = document.createElement("img");
       img.src = "img/sound.png";
       img.alt = "sound";
       img.classList.add("soundState");
       img.addEventListener('click', () => {
-        chrome.tabs.query({index:tab.index}, (innerTabs) => {
+        chrome.tabs.query({ index: tab.index }, (innerTabs) => {
           innerTabs.forEach((innerTab) => {
-            if(innerTab.mutedInfo.muted){
+            if (innerTab.mutedInfo.muted) {
               img.alt = "sound";
               img.src = "img/sound.png";
-            }else{
+            } else {
               img.alt = "nosound";
               img.src = "img/nosound.png";
             }
             let changeMute = innerTab.mutedInfo.muted ? false : true
-            chrome.tabs.update(innerTab.id, {muted: changeMute})
+            chrome.tabs.update(innerTab.id, { muted: changeMute })
           })
         })
-
       })
 
       let div = document.createElement("div")
@@ -49,36 +47,27 @@ let tabArray = chrome.tabs.query({},(tabs) => {
       tabContainer.appendChild(tabDiv)
       container.appendChild(tabContainer)
 
-
-
-
-
       let clickableTitle = document.getElementById(tab.id)
 
       clickableTitle.addEventListener("click", () => {
-        chrome.tabs.query({highlighted:true}, (innerTabs) => {
+        chrome.tabs.query({ highlighted: true }, (innerTabs) => {
           innerTabs.forEach((innerTab) => {
-            chrome.tabs.update(innerTab.id, {highlighted: false})
+            chrome.tabs.update(innerTab.id, { highlighted: false })
           })
         })
-        chrome.windows.update(tab.windowId, {focused:true})
-        chrome.tabs.update(tab.id, {highlighted: true})
+        chrome.windows.update(tab.windowId, { focused: true })
+        chrome.tabs.update(tab.id, { highlighted: true })
       })
-
-
-
-
     }
-
   })
 })
 
 let muteAll = document.getElementById("muteAll");
 
 muteAll.addEventListener("click", () => {
-  chrome.tabs.query({audible:true}, (innerTabs) => {
+  chrome.tabs.query({ audible: true }, (innerTabs) => {
     innerTabs.forEach((innerTab) => {
-      chrome.tabs.update(innerTab.id, {muted:true})
+      chrome.tabs.update(innerTab.id, { muted: true })
     })
   })
   document.querySelectorAll(".soundState").forEach((e) => {
@@ -89,9 +78,9 @@ muteAll.addEventListener("click", () => {
 let unMuteAll = document.getElementById("unMuteAll");
 
 unMuteAll.addEventListener("click", () => {
-  chrome.tabs.query({audible:true}, (innerTabs) => {
+  chrome.tabs.query({ audible: true }, (innerTabs) => {
     innerTabs.forEach((innerTab) => {
-      chrome.tabs.update(innerTab.id, {muted:false})
+      chrome.tabs.update(innerTab.id, { muted: false })
     })
   })
   document.querySelectorAll(".soundState").forEach((e) => {
